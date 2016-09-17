2bbfdba6ebde31cc70998b1eff72c308847a5024
Anil Keshari <@groupon.com>
2016-09-17 22:35:59 +0530

    Add core script to the template

--- src/presenter.js
+++ src/presenter.js
@@ -1,10 +1,24 @@
 const Config = require("../config/top_nav_config.json");
 
-const presenter = ({cookies={}, topNav = true}, page) => {
-  let isLogged = cookies['isLogged'];
+const presenter = ({cookies={}, topNav = true, scriptFile='corejs'}, page, modules={}) => {
+  const {jsAsset, logger} = modules;
+
+  if(!page) {
+    if(logger) {
+      logger.error(`Layout Presenter: Page object not passed`);
+    }
+
+    return;
+  }
+
+  const isLogged = cookies['isLogged'];
 
   if(cookies) {
     page.set({isLogged});
+  }
+
+  if(jsAsset) {
+    page.set({corejs: jsAsset(scriptFile)});
   }
 
   if(topNav) {
