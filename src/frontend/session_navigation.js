c8e29488480de504f5123557481a24ca07028d55
Anil Keshari <@groupon.com>
2016-09-23 01:16:41 +0530

    Remove semi colon

--- src/frontend/session_navigation.js
+++ src/frontend/session_navigation.js
@@ -1,18 +1,18 @@
-import $ from "jquery";
+import $ from "jquery"
 
 const addReferrer = () => {
-  let selectors = '#user-signin a, #user-signup a, #user-signout a';
-  let currentHref = location.href;
+  let selectors = '#user-signin a, #user-signup a, #user-signout a'
+  let currentHref = location.href
 
-  currentHref = currentHref.replace(/[?&]ref_url=([^&]*)?/, '');
+  currentHref = currentHref.replace(/[?&]ref_url=([^&]*)?/, '')
 
-  let referrer = 'ref_url=' + encodeURIComponent(currentHref);
+  let referrer = 'ref_url=' + encodeURIComponent(currentHref)
 
   $(selectors).each(function() {
-    let urlAppender = /\?/.test(this.href) ? '&' : '?';
-    let refUrlString = urlAppender + referrer;
-    this.href += refUrlString;
-  });
+    let urlAppender = /\?/.test(this.href) ? '&' : '?'
+    let refUrlString = urlAppender + referrer
+    this.href += refUrlString
+  })
 }
 
-export default addReferrer;
+export default addReferrer
--- src/presenter.js
+++ src/presenter.js
@@ -1,37 +1,42 @@
-const Config = require("../config/top_nav_config.json");
+const Config = require("../config/top_nav_config.json")
 
 const presenter = ({session={}, topNav = true, scriptFile='core'}, page, modules={}) => {
-  const {jsAsset, logger} = modules;
+  const {jsAsset, logger} = modules
 
   if(!page) {
     if(logger) {
-      logger.error(`Layout Presenter: Page object not passed`);
+      logger.error(`Layout Presenter: Page object not passed`)
     }
 
-    return;
+    return
   }
 
-  const isLogged = session.user !== undefined;
+  const isLogged = session.user !== undefined
 
-  page.set({isLogged});
+  page.set({isLogged})
 
   if(isLogged) {
-    let firstName = session.user.first_name;
-    firstName = firstName.split(" ")[0];
-    page.set({firstName});
+    let firstName = session.user.first_name
+
+    if(firstName) {
+      firstName = firstName.split(" ")[0]
+      page.set({firstName})
+    } else {
+      logger.warn('[WARN] first name is not present for the user')
+    }
   }
 
   if(jsAsset) {
-    page.set({corejs: jsAsset(scriptFile)});
+    page.set({corejs: jsAsset(scriptFile)})
   }
 
   if(topNav) {
-    page.set({origConfig: Config});
+    page.set({origConfig: Config})
   } else {
-    page.set({origConfig: []});
+    page.set({origConfig: []})
   }
 
-  return {isLogged};
+  return {isLogged}
 }
 
-export default presenter;
+export default presenter
